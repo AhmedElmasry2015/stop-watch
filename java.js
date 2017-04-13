66fbdd695f43a10f7b2f231a4e3427fe40d9f680
ahmedelmasry2015 <ahmed.saeed.elmasry@gmail.com>
2017-04-13 19:48:17 +0200

    final draft

---
+++ java.js
@@ -0,0 +1,28 @@
+
+var hour=document.getElementById('hour');
+var minute=document.getElementById('minute');
+var seconds=document.getElementById('seconds');
+var x;
+function go(){
+seconds+=1;
+document.getElementById('seconds').innerHTML=seconds;
+if(seconds>= 59){
+seconds=0;
+document.getElementById('seconds').innerHTML=0;
+minute+=1;
+document.getElementById('minute').innerHTML=minute;
+}
+if(minute >=59){
+	minute=0;
+	hour+=1;
+	document.getElementById('minute').innerHTML=0;
+	document.getElementById('hour').innerHTML=hour;
+}
+}
+
+function start(){
+x=window.setInterval(go,1000);
+}
+function stop(){
+	window.clearInterval(x);
+}
